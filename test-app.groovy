d17f7d5b2f7c1aebd516ea0cd2ebd21a225a9245
twack <todd@wackford.net>
2019-12-07 09:18:35 -0800

    Create test-app.groovy

---
+++ test-app.groovy
@@ -0,0 +1,8 @@
+definition(
+    name: "test-app",
+    namespace: "smartport",
+    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.2"}',
+    description: "The Description",
+    category: "",
+    iconUrl: "",
+    iconX2Url: "")
