e5fa318a22cf459376be76ee7baab1cc98d3b2f0
twack <todd@wackford.net>
2019-12-09 06:23:10 -0800

    Update test-app.groovy

--- test-app.groovy
+++ test-app.groovy
@@ -2,7 +2,7 @@ definition(
     name: "test-app",
     namespace: "smartport",
     author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.2"}',
-    description: "The Description",
+    description: "The Version 0.0.2 Description",
     category: "",
     iconUrl: "",
     iconX2Url: "")
