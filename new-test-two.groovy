df41323c2e136c0c143fa0a903a0dc6312514a00
twack <todd@wackford.net>
2019-12-09 06:37:43 -0800

    Create new-test-two.groovy

---
+++ new-test-two.groovy
@@ -0,0 +1,8 @@
+definition(
+    name: "new-test-two",
+    namespace: "smartport",
+    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.1"}',
+    description: "The Version 0.0.1 Description",
+    category: "",
+    iconUrl: "",
+    iconX2Url: "")
