03f9e2a7a503d5462a1323aa88b951665ab2709e
twack <todd@wackford.net>
2019-12-09 07:47:30 -0800

    Update new-test-two.groovy

--- new-test-two.groovy
+++ new-test-two.groovy
@@ -1,8 +1,8 @@
 definition(
     name: "new-test-two",
     namespace: "smartport",
-    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.2"}',
-    description: "The Version 0.0.2 Description",
+    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.3"}',
+    description: "The Version 0.0.3 Description",
     category: "",
     iconUrl: "",
     iconX2Url: "")
