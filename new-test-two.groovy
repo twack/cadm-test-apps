add04f4a4d2b20ccad1911d6c8d4e6b046620626
twack <todd@wackford.net>
2019-12-10 16:09:19 -0800

    Delete new-test-two.groovy

--- new-test-two.groovy
+++
@@ -1,8 +0,0 @@
-definition(
-    name: "new-test-two",
-    namespace: "smartport",
-    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.3"}',
-    description: "The Versions 0.0.3 Description",
-    category: "",
-    iconUrl: "",
-    iconX2Url: "")
