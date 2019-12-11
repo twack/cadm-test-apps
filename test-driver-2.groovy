4d36827686f119a61622326d9ec4f5ae97eff54c
twack <todd@wackford.net>
2019-12-10 16:20:12 -0800

    Create test-driver-2.groovy

---
+++ test-driver-2.groovy
@@ -0,0 +1,13 @@
+metadata 
+{
+	definition(
+		name: "test-driver-2", 
+		namespace: "smartport",
+		author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.1"}', 
+		category: "this is the driver desciption"
+	)
+	{
+		capability "Refresh"
+		capability "Battery"
+	}
+}
