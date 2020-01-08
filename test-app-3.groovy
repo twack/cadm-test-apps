cd7f975f987c8c7c2d8fd7bfb656c4eb5143108d
twack <todd@wackford.net>
2020-01-08 12:16:17 -0800

    Delete test-app-3.groovy

--- test-app-3.groovy
+++
@@ -1,8 +0,0 @@
-definition(
-    name: "test-app-3",
-    namespace: "smartport",
-    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.1"}',
-    description: "The Version Description",
-    category: "",
-    iconUrl: "",
-    iconX2Url: "")
