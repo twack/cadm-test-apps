74b0a77bf0fad152d5b97077b2a20df50ca83edd
twack <todd@wackford.net>
2020-01-08 12:16:10 -0800

    Delete test-app-2.groovy

--- test-app-2.groovy
+++
@@ -1,8 +0,0 @@
-definition(
-    name: "test-app-2",
-    namespace: "smartport",
-    author: '{"authorName": "Todd Wackford", "smartportVersion": "0.0.1"}',
-    description: "The Version Description",
-    category: "",
-    iconUrl: "",
-    iconX2Url: "")
